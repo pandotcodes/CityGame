1fd19f9fac63cd404e4c6c60ebc953c830ad8bc5
Jonathan Riedel <pn@bptv.eu>
2023-05-10 23:55:22 +0200

    UI things

--- CityGame/Program.cs
+++ CityGame/Program.cs
@@ -1,26 +1,30 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using WPFGame;
 
 namespace CityGame
 {
     public class Program { 
         public static void Main() {
-            var menu = new MainWindow();
+            var menu = new MenuWindow();
         } 
     }
     public class MenuWindow : Window
     {
-        private GraphicsDevice graphics;
         public MenuWindow()
         {
-            
+            UICanvas = new Canvas();
+            UICanvas.Children.Add(new ReflectedUIWindow(new GenerationSettings(), "CNRGN  Builder", 24) { Height = 1080, Width = 300 });
 
             Show();
         }
     }
     public class GenerationSettings
     {
-        public int Seed;
+        [RUITextField(TextColor = 0xFFFF0000, Lines = 2)]
+        public string WelcomeText { get; set; } = "Welcome!\nTest";
+        [RUITextField(TextColor = 0xFF00FF00, Lines = 2)]
+        public int Seed { get; set; } = 0;
         public GenerationSettings()
         {
 
